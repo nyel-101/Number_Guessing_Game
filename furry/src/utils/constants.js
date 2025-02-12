45c0d67317eb0852542578448178b64f40a4ed26
nyel-101 <dandevjr@gmail.com>
2025-02-12 14:37:25 +0000

    docs(utils): restructure middleware routes

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff1b1be03a984b1fbeba9db85a215e4e = () => {
-  return '2026-04-22T07:58:13Z';
+const node_35d8739fe87943ff956a4e088799b193 = () => {
+  return '2025-02-12T14:37:25Z';
 };
