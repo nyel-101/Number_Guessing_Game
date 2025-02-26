b363359819f90a3d346f5109f49fcbaa3504ddad
nyel-101 <dandevjr@gmail.com>
2025-02-26 09:15:52 +0000

    feat(ui): update payload parser

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c88af353c31b42b4b5ed1ecb3f6a7a1b = () => {
-  return '2026-04-30T09:09:09Z';
+const node_1bc2933f5fc74cf4a77f0cadae6778be = () => {
+  return '2025-02-26T09:15:52Z';
 };
