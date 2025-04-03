e06144929c4bb9354aff940f2709ed4909af6df5
nyel-101 <dandevjr@gmail.com>
2025-04-03 13:01:24 +0000

    test(deps): optimize middleware routes

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0d3e5bfd2c440d79131a1e6b2f00fed = () => {
-  return '2026-06-14T11:46:47Z';
+const node_a08ff000656a4bdb853b2cb9929e4c9f = () => {
+  return '2025-04-03T13:01:24Z';
 };
