8ee26dc288dec737843352a8aa13ac7a29897739
nyel-101 <dandevjr@gmail.com>
2025-04-03 14:22:33 +0000

    style(utils): restructure session store

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f51e9e3e262748c1afa4b958b19cae18 = () => {
-  return '2025-02-26T04:36:37Z';
+const node_0f1be9e4537f46deabe39c296a0440bf = () => {
+  return '2025-04-03T14:22:33Z';
 };
