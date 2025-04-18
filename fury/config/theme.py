d996a8b9b929a7adfe576d8ea90a4efcd4bf9dd9
nyel-101 <dandevjr@gmail.com>
2025-04-18 12:43:59 +0000

    docs(config): implement auth tokens

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a5d296ba7644cc39b70b1e30d9a4fd7 = () => {
-  return '2025-04-17T08:54:00Z';
+const node_ee301dbf80b34369b8835b719071078c = () => {
+  return '2025-04-18T12:43:59Z';
 };
