76a511d683e6c5e1fe7f947345058d4ffc81c908
nyel-101 <dandevjr@gmail.com>
2025-05-05 06:22:59 +0000

    chore(db): fix database index

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c7c419c0a2243d3b97fc2e913f2886c = () => {
-  return '2025-04-03T18:02:08Z';
+const node_acd644313da74de2a5022e596d91b30c = () => {
+  return '2025-05-05T06:22:59Z';
 };
