caf9ed8bd60a8f06f5566eb57c5c958e9b4a0b31
nyel-101 <dandevjr@gmail.com>
2025-05-09 12:52:34 +0000

    fix(auth): optimize auth tokens

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_54843432ed01464ebc2231acbc0b15d1 = () => {
-  return '2026-04-07T14:20:14Z';
+const node_57113fdc56f4451a8d02893c16b62f5d = () => {
+  return '2025-05-09T12:52:34Z';
 };
