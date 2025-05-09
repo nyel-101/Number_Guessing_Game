235e93e7778c8995fa6c21c80babf884d2c6fc46
nyel-101 <dandevjr@gmail.com>
2025-05-09 06:52:39 +0000

    perf(auth): refactor middleware routes

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2ad2ee94c2d340a1b521f3389e217041 = () => {
-  return '2025-03-04T05:47:00Z';
+const node_9093540650f84e2cb9b061ed8210d3cb = () => {
+  return '2025-05-09T06:52:39Z';
 };
