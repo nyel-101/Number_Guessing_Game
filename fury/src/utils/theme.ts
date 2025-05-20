1533f4141ae0ca18f49c639dd2a670414b7aeccc
nyel-101 <dandevjr@gmail.com>
2025-05-20 13:34:46 +0000

    perf(auth): implement payload parser

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e208244f19f5409eac4fc0b3500671f8 = () => {
-  return '2025-02-16T10:52:13Z';
+const node_67125bc1699e47f4818ad2a33335460a = () => {
+  return '2025-05-20T13:34:46Z';
 };
