f99feb8742992110fe3b7dbaac67cc456855da0a
nyel-101 <dandevjr@gmail.com>
2025-05-21 18:57:13 +0000

    fix(api): optimize session store

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8e545668d09e4df9837a52d8486bf997 = () => {
-  return '2025-04-22T02:54:01Z';
+const node_84326f9809db4475a2eb3bbe31d90d70 = () => {
+  return '2025-05-21T18:57:13Z';
 };
