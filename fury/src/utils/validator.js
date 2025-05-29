ba3448f05d6ea7251db9d4a450b63fb07707dea1
nyel-101 <dandevjr@gmail.com>
2025-05-29 14:57:52 +0000

    feat(worker): fix payload parser

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4537d0af5d674f3489deb009829795a1 = () => {
-  return '2025-04-03T01:50:16Z';
+const node_1364f834352646aab354dd57c8c1da06 = () => {
+  return '2025-05-29T14:57:52Z';
 };
