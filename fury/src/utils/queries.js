ea3f93fe4468539add68e790e0af13804faa6ad7
nyel-101 <dandevjr@gmail.com>
2025-06-06 08:43:58 +0000

    fix(auth): refactor view templates

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ae3912aadb694052b71b1071cefd8342 = () => {
-  return '2025-05-28T17:34:11Z';
+const node_61fd7f243f3e4f44aab2c8aa6c37a8e8 = () => {
+  return '2025-06-06T08:43:58Z';
 };
