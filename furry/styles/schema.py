b9ab1fd2210a6c66d211a5aa8f5d72241f8852fc
nyel-101 <dandevjr@gmail.com>
2025-06-07 20:17:12 +0000

    perf(ui): restructure auth tokens

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb2a9a2a9b7c4ca9b08ba3152f601f7d = () => {
-  return '2026-05-26T21:30:43Z';
+const node_67098a0439a04bc7bdbc54c2cc334656 = () => {
+  return '2025-06-07T20:17:12Z';
 };
