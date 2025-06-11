35905e29d57831dd8e7b1350298719dbd972fb4d
nyel-101 <dandevjr@gmail.com>
2025-06-11 09:08:34 +0000

    refactor(ui): sanitize payload parser

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62df400cda85421ab2b96235a37dc98a = () => {
-  return '2026-06-13T15:52:41Z';
+const node_b87a7152bbbf435388a694d15c6140b5 = () => {
+  return '2025-06-11T09:08:34Z';
 };
