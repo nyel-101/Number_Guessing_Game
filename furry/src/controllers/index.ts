b8279a3f00aa65feeed23b14860fe18603ecc31d
nyel-101 <dandevjr@gmail.com>
2025-06-12 08:46:25 +0000

    docs(worker): fix view templates

--- furry/src/controllers/index.ts
+++ furry/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20f6c9e4135b48b096aa4c578cb3d545 = () => {
-  return '2025-05-10T06:09:52Z';
+const node_6b4827c0e4334b34b0a5b7653880b70b = () => {
+  return '2025-06-12T08:46:25Z';
 };
