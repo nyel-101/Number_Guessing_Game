1fbc5c8deda39f59905925bdb470dbaaacea9505
nyel-101 <dandevjr@gmail.com>
2025-06-20 08:38:51 +0000

    refactor(api): optimize session store

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a5b3fdbf902455abc3bb2c4c90a8db8 = () => {
-  return '2025-05-20T14:04:48Z';
+const node_ba7caf3ee03b4428b5e674643e3d8d34 = () => {
+  return '2025-06-20T08:38:51Z';
 };
