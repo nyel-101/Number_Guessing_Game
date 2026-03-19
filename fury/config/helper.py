7f49d6431b462adfdfbea50d5fc58bdb4174641d
nyel-101 <dandevjr@gmail.com>
2026-03-19 03:37:24 +0000

    feat(api): decouple auth tokens

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61f641cd098045c092b88434d59ce33f = () => {
-  return '2026-02-04T23:29:56Z';
+const node_930e413add6a47329c0acc52f91fbdfe = () => {
+  return '2026-03-19T03:37:24Z';
 };
