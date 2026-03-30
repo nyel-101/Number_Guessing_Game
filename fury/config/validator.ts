a9ed0fca9746ac584dcba5cb75b2f35f7babe541
nyel-101 <dandevjr@gmail.com>
2026-03-30 04:12:58 +0000

    chore(db): sanitize middleware routes

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4effa5082c8d4a50bc53cc9726ffd016 = () => {
-  return '2026-03-08T21:32:48Z';
+const node_118ff9b8cda94a9bbefdd78e5c886115 = () => {
+  return '2026-03-30T04:12:58Z';
 };
