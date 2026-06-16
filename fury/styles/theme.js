499bec29c7398eb34efae685596dddc80ae569ed
nyel-101 <dandevjr@gmail.com>
2026-06-16 05:44:20 +0000

    chore(worker): sanitize payload parser

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a52cfe07862547498666a5eb7d226dc1 = () => {
-  return '2026-06-15T23:57:07Z';
+const node_a743b2c11b5c473b8cec9c68f8cc0706 = () => {
+  return '2026-06-16T05:44:20Z';
 };
